bb64145058d9743a23ca6d79e5639898eaa0df9d
RYOSUKE ISHIZUE <ryoskate@gmail.com>
2017-05-12 14:28:26 +0900

    aojRank.csvの読み込み処理追加 ヘッダと1行目の例 id,rank,name,date "10000","1","Hello World","4月10日"

--- ScrapingAOJ/app.js
+++ ScrapingAOJ/app.js
@@ -3,63 +3,76 @@ const csv = require('csv');
 const iconv = require('iconv-lite');
 const Nightmare = require('nightmare');
 
-const inputFilename = (2 in process.argv) ? process.argv[2] : 'aoj.txt';
-const outputFilename = (3 in process.argv) ? process.argv[3] : 'result.csv';
+const inputFilename = (2 in process.argv) ? process.argv[2] : 'aoj.csv';
+const inputFilenameRank = (3 in process.argv) ? process.argv[3] : 'aojRank.csv';
+const outputFilename = (4 in process.argv) ? process.argv[4] : 'result.csv';
+
 
 fs.readFile(inputFilename, (err, data) => {
     // オプションに{columns:true}をつけると、1行目をプロパティ名にしたオブジェクトの配列が返る
     csv.parse(data, { columns: true }, (err, output) => {
 
-        var array = new Array();
+        var arrayCSV = new Array();
 
         output.forEach((value) => {
-            array.push(value);
+            arrayCSV.push(value);
         });
 
-        var count = 0;
-        fs.writeFile(outputFilename, '学籍番号,出席簿番号,AOJ\n');
-        var getAOJ = function () {
-            if (array.length <= count) {
-                return 0;
-            }
-            const value = array[count++];
-            const number = value.number;
-            const URL = value.account + "#2";
-            const syussekibo = value.syussekibo;
+        fs.readFile(inputFilenameRank, (err, data) => {
+            // オプションに{columns:true}をつけると、1行目をプロパティ名にしたオブジェクトの配列が返る
+            csv.parse(data, { columns: true }, (err, output) => {
 
+                var arrayRank = new Array();
 
-            var nightmare = Nightmare(
-                {
-                    show: false,
-                    //waitTimeout: 30000,
-                    //gotoTimeout: 30000,
-                    //loadTimeout: 30000,
-                    //executionTimeout: 30000
+                output.forEach((value) => {
+                    arrayRank.push(value);
                 });
+                var count = 0;
+                fs.writeFile(outputFilename, '学籍番号,出席簿番号,AOJ\n');
+                var getAOJ = function () {
+                    if (arrayCSV.length <= count) {
+                        return 0;
+                    }
+                    const value = arrayCSV[count++];
+                    const number = value.number;
+                    const URL = value.account + "#2";
+                    const syussekibo = value.syussekibo;
 
-            nightmare
-                .goto(URL)
-                .wait('#pager_right')
-                .evaluate(function () {
-                    const element = document.querySelector('#pager_right div');
-                    const innerHTML = element.innerHTML;
-                    const numOfSolved = innerHTML.substr(innerHTML.lastIndexOf(' ')).trim();
-                    return numOfSolved;
-                })
-                .end()
-                .then(function (result) {
-                    const text = number + ',' + syussekibo + ',' + result;
-                    fs.appendFile(outputFilename, text + '\n');
-                    console.log(count + ":" + text);
-                    getAOJ();
-                })
-                .catch(function (error) {
-                    const text = number + ',' + syussekibo + ',' + value.account;
-                    fs.appendFile(outputFilename, text + '\n');
-                    console.error(count + ":" + number + ',' + URL + ',' + 'Search failed:', error);
-                    getAOJ();
-                });
-        }
-        getAOJ();
+
+                    var nightmare = Nightmare(
+                        {
+                            show: true,
+                            //waitTimeout: 30000,
+                            //gotoTimeout: 30000,
+                            //loadTimeout: 30000,
+                            //executionTimeout: 30000
+                        });
+
+                    nightmare
+                        .goto(URL)
+                        .wait('#pager_right')
+                        .evaluate(function () {
+                            const element = document.querySelector('#pager_right div');
+                            const innerHTML = element.innerHTML;
+                            const numOfSolved = innerHTML.substr(innerHTML.lastIndexOf(' ')).trim();
+                            return numOfSolved;
+                        })
+                        .end()
+                        .then(function (result) {
+                            const text = number + ',' + syussekibo + ',' + result;
+                            fs.appendFile(outputFilename, text + '\n');
+                            console.log(count + ":" + text);
+                            getAOJ();
+                        })
+                        .catch(function (error) {
+                            const text = number + ',' + syussekibo + ',' + value.account;
+                            fs.appendFile(outputFilename, text + '\n');
+                            console.error(count + ":" + number + ',' + URL + ',' + 'Search failed:', error);
+                            getAOJ();
+                        });
+                }
+                getAOJ();
+            })
+        });
     })
 });
